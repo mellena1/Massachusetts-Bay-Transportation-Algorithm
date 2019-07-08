e26e9c9c56a1dcf84ac9b0d32b5a20702f95a5ac
charles <charlesarnaudo@gmail.com>
2019-07-08 15:21:32 -0400

    change stop

--- mbta/stop.go
+++ mbta/stop.go
@@ -2,12 +2,12 @@ package mbta
 
 // Stop represents an mbta stop
 type Stop struct {
-	ID      string   `json:"id"`
-	Name    string   `json:"name"`
-	EdgeIDs []string `json:"edgeIDs"`
+	ID    string  `json:"id"`
+	Name  string  `json:"name"`
+	Edges []*Stop `json:"edges"`
 }
 
 // IsEndpoint returns true if the stop is on the end of a line
 func (s *Stop) IsEndpoint() bool {
-	return len(s.EdgeIDs) == 1
+	return len(s.Edges) == 1
 }
