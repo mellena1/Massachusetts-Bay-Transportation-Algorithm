c39d063c25267a477bb04d2d2f959bf9f4235e1f
charles <charlesarnaudo@gmail.com>
2019-07-08 16:12:20 -0400

    isIntersection

--- mbta/stop.go
+++ mbta/stop.go
@@ -1,5 +1,12 @@
 package mbta
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
 // Stop represents an mbta stop
 type Stop struct {
 	ID    string  `json:"id"`
@@ -10,4 +17,33 @@ type Stop struct {
 // IsEndpoint returns true if the stop is on the end of a line
 func (s *Stop) IsEndpoint() bool {
 	return len(s.Edges) == 1
+}
+
+// IsIntersection returns true if stop is an intersecting stop
+func (s *Stop) IsIntersection() bool {
+	return len(s.Edges) > 1
+}
+
+// UnmarshalJSON customer unmarshaller for the graph
+func (s *Stop) UnmarshalJSON(b []byte) error {
+	file, err := os.Open("graph.json")
+	if err != nil {
+		log.Fatal("Could not load file graph.json")
+	}
+
+	type tempStop struct {
+		ID    string    `json:"id"`
+		Name  string    `json:"name"`
+		Edges []*string `json:"edges"`
+	}
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal("Could not read bytes from file graph.json")
+	}
+
+	var tempStops []tempStop
+	json.Unmarshal(data, &stops)
+
+	return nil
 }
