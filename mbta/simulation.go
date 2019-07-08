ddcb4f504fce27d679783e23db9cd8f4bd4add7e
charles <charlesarnaudo@gmail.com>
2019-07-08 16:37:33 -0400

    stop stuff

--- mbta/simulation.go
+++ mbta/simulation.go
@@ -2,35 +2,39 @@ package mbta
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
 func main() error {
-	stops, err := importGraph()
+	err := importGraph("graph.json")
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(stops)
-
 	return nil
 }
 
-func importGraph() (*[]Stop, error) {
-	jsonFile, err := os.Open("graph.json")
+func importGraph(filename string) error {
+	file, err := os.Open(filename)
 	if err != nil {
-		return nil, err
+		log.Fatalf("Could not load file %s", filename)
 	}
 
-	data, err := ioutil.ReadAll(jsonFile)
+	bytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		return nil, err
+		log.Fatalf("Could not read bytes from %s", filename)
 	}
 
-	var stops []Stop
-	json.Unmarshal(data, &stops)
+	type tempStop struct {
+		ID    string   `json:"id"`
+		Name  string   `json:"name"`
+		Edges []string `json:"edges"`
+	}
 
-	return &stops, nil
+	var tempStops []tempStop
+	json.Unmarshal(bytes, &tempStops)
+
+	return nil
 }
--- mbta/stop.go
+++ mbta/stop.go
@@ -1,12 +1,5 @@
 package mbta
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"os"
-)
-
 // Stop represents an mbta stop
 type Stop struct {
 	ID    string  `json:"id"`
@@ -22,28 +15,4 @@ func (s *Stop) IsEndpoint() bool {
 // IsIntersection returns true if stop is an intersecting stop
 func (s *Stop) IsIntersection() bool {
 	return len(s.Edges) > 1
-}
-
-// UnmarshalJSON customer unmarshaller for the graph
-func (s *Stop) UnmarshalJSON(b []byte) error {
-	file, err := os.Open("graph.json")
-	if err != nil {
-		log.Fatal("Could not load file graph.json")
-	}
-
-	type tempStop struct {
-		ID    string    `json:"id"`
-		Name  string    `json:"name"`
-		Edges []*string `json:"edges"`
-	}
-
-	bytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		log.Fatal("Could not read bytes from file graph.json")
-	}
-
-	var tempStops []tempStop
-	json.Unmarshal(data, &stops)
-
-	return nil
 }
